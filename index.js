f06df9bc861ea1e3fd3ff348fea590188674d99b
Nicholas Broström <nicholas.brostrom@effectsoft.se>
2020-06-10 19:42:54 +0200

    Plays youtube songs now :)

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const {
   Client
 } = require('discord.js');
+const ytdl = require('ytdl-core');
 require('dotenv').config();
 const client = new Client();
 const fs = require('fs');
 const path = require('path');
+const queue = new Map();
+
+//Statics
+const prefix = '!';
+const regYoutube = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+
+//Sound files
 const fileMap = new Map();
 const files = fs.readdirSync('JoinSounds');
 files.forEach((file, index) => {
@@ -12,8 +20,11 @@ files.forEach((file, index) => {
   fileMap.set(index, fileName);
 })
 
+//States
 let isReady = true;
 let weirdchampStatus = true;
+
+//When user is ready
 client.on('ready', () => {
   client.user.setActivity('!weirdchamp')
     .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
@@ -21,7 +32,12 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+
+//Various on message commands.
+
 client.on('message', msg => {
+  if (msg.author.bot) return;
+
   if (msg.content === '!weirdchamp') {
     msg.reply('https://tenor.com/view/weird-champ-weird-champ-pogchamp-pog-gif-13780848');
   }
@@ -41,8 +57,26 @@ client.on('message', msg => {
       msg.react(weirdchamp);
     }
   }
+
+  if (!msg.content.startsWith(prefix)) return;
+
+  const serverQueue = queue.get(msg.guild.id);
+
+  if (msg.content.startsWith(`${prefix}play`)) {
+    execute(msg, serverQueue);
+    return;
+  } else if (msg.content.startsWith(`${prefix}skip`)) {
+    skip(msg, serverQueue);
+    return;
+  } else if (msg.content.startsWith(`${prefix}stop`)) {
+    stop(msg, serverQueue);
+    return;
+  } else {
+    msg.channel.send("Not a valid command! <:weird:668843974504742912>");
+  }
 });
 
+//React to someone reacting with :Pet:
 client.on('messageReactionAdd', msgRect => {
   if (weirdchampStatus) {
     if (msgRect.emoji.name == 'pet') {
@@ -51,6 +85,7 @@ client.on('messageReactionAdd', msgRect => {
   }
 })
 
+//Join when a user joins play random sounds
 client.on('voiceStateUpdate', async (oldMember, newMember) => {
   let newUserChannel = newMember.channel;
   let oldUserChannel = oldMember.channel;
@@ -76,8 +111,118 @@ client.on('voiceStateUpdate', async (oldMember, newMember) => {
   }
 });
 
+//Client login
 client.login(process.env.DISCORD_KEY);
 
+//Youtube functions
+async function execute(message, serverQueue) {
+  const args = message.content.split(" ");
+
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel)
+    return message.channel.send(
+      "You're not in a voice channel! <:weird:668843974504742912>"
+    );
+  const permissions = voiceChannel.permissionsFor(message.client.user);
+  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+    return message.channel.send(
+      "No permission <:weird:668843974504742912>"
+    );
+  }
+  if (!args[1].match(regYoutube)){
+    return message.channel.send(
+      "This is not valid fucking youtube link! <:weird:668843974504742912>"
+    );
+  }
+  const songInfo = await ytdl.getInfo(args[1]);
+  const song = {
+    title: songInfo.title,
+    url: songInfo.video_url
+  };
+
+  if (!serverQueue) {
+    const queueContruct = {
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      songs: [],
+      volume: 5,
+      playing: true
+    };
+
+    queue.set(message.guild.id, queueContruct);
+
+    queueContruct.songs.push(song);
+
+    try {
+      var connection = await voiceChannel.join();
+      queueContruct.connection = connection;
+      play(message.guild, queueContruct.songs[0]);
+    } catch (err) {
+      console.log(err);
+      queue.delete(message.guild.id);
+      return message.channel.send(err);
+    }
+  } else {
+    serverQueue.songs.push(song);
+    return message.channel.send(`${song.title} has been added to the queue! <:Happy:711247709729718312>`);
+  }
+}
+
+function skip(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "Join a voice channel to skip the music! <:pepega:709781824771063879>"
+    );
+  if (!serverQueue)
+    return message.channel.send("No song to skip! <:pepega:709781824771063879>");
+  serverQueue.connection.dispatcher.end();
+}
+
+function stop(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "Join a voice channel to stop the music! <:pepega:709781824771063879>"
+    );
+    if (!serverQueue)
+      return message.channel.send(
+        "Something went fucking wrong! <:pepelaugh:699711830523773040>"
+      );
+  serverQueue.songs = [];
+  serverQueue.connection.dispatcher.end();
+}
+
+function play(guild, song) {
+  const serverQueue = queue.get(guild.id);
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+  if (song.url == null){
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+
+  if (!song.url.match(regYoutube)) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+
+  const dispatcher = serverQueue.connection
+      .play(ytdl(song.url))
+      .on("finish", () => {
+        serverQueue.songs.shift();
+        play(guild, serverQueue.songs[0]);
+      })
+      .on("error", error => console.error(error));
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    serverQueue.textChannel.send(`Start playing: **${song.title}** <:pog:710437255231176764>`);
+}
+
+//Utility functions
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
