e99d98a584027e05a55087c0670f03501f00114d
Nicholas Broström <nicholas.brostrom@effectsoft.se>
2020-08-11 13:20:03 +0200

    origin undefined 😇

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var whitelist = [
 ];
 var corsOptions = {
     origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
         } else {
             callback(new Error(`Not allowed by CORS. ORIGIN: ${origin}`));
